2e72e670afa2f6f387ead17c5b6ee22e2b11fa39
Erion <erioncriscente@gmail.com>
2020-09-06 13:29:23 -0300

    Variável ajustada no heruko

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://ecr07-backend-grades.herokuapp.com',
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     'Content-type': 'application/json',
   },
